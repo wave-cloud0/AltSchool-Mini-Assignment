5ab0455867dfa4d65fb3a48dfff1509d00545709
wʌve <130824151+wave-cloud0@users.noreply.github.com>
2023-10-29 17:53:25 -0400

    slave.sh

--- slave.sh
+++ slave.sh
@@ -3,118 +3,84 @@
 echo "Provisioning slave..."
 
 
-   
-# Install software, configure settings, etc_
-# Update the package manager_
-   
+## Update the package manager
 sudo apt update
 
 
-   
-# Upgrade applications_
-   
+## Upgrade applications
 sudo apt upgrade -y
 echo "Update and upgrade done"
 
 
-   
-# Install Ifconfig_
-   
+## Install Ifconfig
 sudo apt install net-tools
 
 
-   
-# Install the SSH server_
-   
+## Install the SSH server
 sudo apt-get install -y openssh-server
 
 
-   
-# Install Apache web server and make it start on boot_
-   
+## Install Apache web server and make it start on boot
 sudo apt install -y apache2
 sudo apachectl -k start 
 
-   
-# Define the path to the sshd_config file_
-   
+
+## Define the path to the sshd_config file
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 sshd_config="/etc/ssh/sshd_config"
 
 
-   
-# Define the new setting for PasswordAuthentication (yes or no)_
-   
+## Define the new setting for PasswordAuthentication (yes or no)
 new_password_auth="yes"
 
 
-   
-# Define the new setting for PubkeyAuthentication (yes or no)_
-   
+## Define the new setting for PubkeyAuthentication (yes or no)
 new_pubkey_authentication="yes"
 
 
-   
-# Use sed to modify the PasswordAuthentication setting in the sshd_config file_
-   
+## Use sed to modify the PasswordAuthentication setting in the sshd_config file
 sed -i "s/^PasswordAuthentication.*/PasswordAuthentication $new_password_auth/" "$sshd_config"
 
 
-   
-# Use sed to modify the PubkeyAuthentication setting in the sshd_config file_
-   
+## Use sed to modify the PubkeyAuthentication setting in the sshd_config file
 sed -i "s/^PubkeyAuthentication.*/PubkeyAuthentication $new_pubkey_authentication/" "$sshd_config"
 
 
-   
-# Restart the SSH service to apply the changes_
-   
+## Restart the SSH service to apply the changes
+
 sudo systemctl restart sshd
 
 
-   
-# Install MySQL server and set root password_
-   
+## Install MySQL server and set root password
 sudo apt install -y mysql-server
 
 
-   
-# Start the MySQL Server and redirect standard error ouput to file_
-   
+## Start the MySQL Server and redirect standard error ouput to file
 echo "Starting MySQL server for the first time"
 
 sudo systemctl start mysql 2> /dev/null
 
 
-   
-# Secure MySQL installation with the options set in the response.txt file_
-   
-sudo mysql_secure_installation < response.txt
+## Secure MySQL installation with the options set in the fix.txt file
+sudo mysql_secure_installation < fix.txt
 
 
-   
-# Confirm Installation Status_
-   
+## Confirm Installation Status
 echo "MySQL installation and security configuration completed."
 
 
-   
-# Install PHP and required modules_
-   
+## Install PHP and required modules
 sudo apt install -y php libapache2-mod-php php-mysql
 
 
-   
-# Enable Apache modules_
-   
+## Enable Apache modules
+
 a2enmod php7.4
 sudo systemctl reload apache2
 
 
-   
-# Create a PHP test file to verify the installation_
-   
+## Create a PHP test file to verify the installation
 sudo echo "<?php
 
 // Show all information, defaults to INFO_ALL
@@ -127,19 +93,15 @@ phpinfo(INFO_MODULES);
 ?>" > /var/www/html/index.php
 
 
-   
-# Reload Apache to apply changes_
-   
+## Reload Apache to apply changes
 sudo systemctl reload apache2
 
 
-   
-# Display a message indicating the LAMP stack is installed_
-   
+## Display a message indicating the LAMP stack is installed
+
 echo "LAMP stack (Apache, MySQL, PHP) has been successfully installed."
 
 
-   
-# Clean up and exit_
-   
+## Clean up and exit
+
 exit 0
